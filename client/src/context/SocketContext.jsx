2437bd7a27c461edca858c2c5f731f738d28aaa5
Stark <inventorstark7@gmail.com>
2024-05-06 23:46:41 +0100

    chore: update api endpoints

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -9,7 +9,7 @@ export const SocketContextProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    setSocket(io("http://localhost:4000"));
+    setSocket(io(`${process.env.REACT_SOCKET_URL}`));
   }, []);
 
   useEffect(() => {
